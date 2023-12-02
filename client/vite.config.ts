1611fd35ea6b7a7d12c4b85c0cbae57c7338b3eb
Jonas <jonasbraga2001@gmail.com>
2023-12-02 20:03:16 -0300

    feat: :sparkles: adding server folder
    and docker compose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     // Importando os estilos do Material-UI
+    //@ts-ignore
     `import "@mui/material/styles";`,
   ],
   optimizeDeps: {
     // Excluindo o makeStyles do @mui/system para evitar problemas de tipos
-    exclude: ['@mui/system/makeStyles'],
+    exclude: ["@mui/system/makeStyles"],
   },
 });
