7a97b2735ab9f1aeb7865e2c3e691166f501f5aa
Jonas <jonasbraga2001@gmail.com>
2023-12-04 10:46:17 -0300

    feat: :sparkles: adding backend

---
+++ server/src/database/connections.ts
@@ -0,0 +1,16 @@
+import knex from "knex";
+
+//Config to connect to the database
+let db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    //@ts-ignore
+    port: Number.parseInt(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+export default db;
---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import routes from "./routes";
+import cors from "cors";
+
+//Express create an API
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+//Accept the routes at 'routes.ts'
+app.use(routes);
+
+//Port used
+app.listen(3333, () => {
+  console.log("Server started on port 3333!");
+});
